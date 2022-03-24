99a1bb1c478517548841fe2735d1b47a6bcf789c
janondracek1 <jan.ondracek1@gmail.com>
2022-03-24 15:53:52 +0100

    Fix error where second parameter was missing

--- src/SevenInteractive/Model/Repository/DayRepository.php
+++ src/SevenInteractive/Model/Repository/DayRepository.php
@@ -28,7 +28,7 @@ abstract class DayRepository extends BaseAssetRepository
                 'midnight' => $midnight,
                 'secondBeforeMidnightNextDay' => $secondBeforeMidnightNextDay
             ])
-            ->orderBy('d.name ASC');
+            ->orderBy('d.name', 'ASC');
 
         if($name !== null){
             $qb
